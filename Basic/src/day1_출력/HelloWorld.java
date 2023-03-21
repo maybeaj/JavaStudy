6c9ec4f023b363f6efdf8c7ac36bd4d833406cda
iuejeong <dmlgus3585@gmail.com>
2023-03-21 23:02:33 +0900

    test4

--- Basic/src/day1_출력/HelloWorld.java
+++ Basic/src/day1_출력/HelloWorld.java
@@ -3,13 +3,9 @@ package day1_출력;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("안녕하세요!");
-=======
 		System.out.println("안녕하세요!!");
 		System.out.println("hj");
 		System.out.println("iuejeong");
->>>>>>> master
 	}
 
 }
