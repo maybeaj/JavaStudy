8db73dedbba12177cfde5caa720266ad37dc3602
iuejeong <dmlgus3585@gmail.com>
2023-03-21 22:56:26 +0900

    test 2차

--- Basic/src/day1_출력/HelloWorld.java
+++ Basic/src/day1_출력/HelloWorld.java
@@ -3,9 +3,6 @@ package day1_출력;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("안녕하세요!!");
-		System.out.println("깃 허브 업로드");
-		System.out.println("iuejeong");
 	}
 
 }
